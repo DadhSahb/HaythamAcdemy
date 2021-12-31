fca13359f6454a567fdd6180cbc5c0bdf84f464b
Hamza Hamid <88159702+DadhSahb@users.noreply.github.com>
2021-12-31 23:26:53 +0500

    Add files via upload

---
+++ js/youVid.js
@@ -0,0 +1,7 @@
+// var iframe = document.createElement('iframe');
+// iframe.src = 'https://www.youtube.com/embed/9kd-PoHWah8';
+// iframe.width = '100';
+// iframe.height = '100';
+
+// var bottom = document.getElementById('youtubeVideo');
+// bottom.appendChild(iframe);
